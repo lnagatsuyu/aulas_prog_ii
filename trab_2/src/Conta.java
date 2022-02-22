852cebb5f808bd81233a9506a5191025d28df767
lnagatsuyu <leonardo.nagatsuyu@gmail.com>
2022-02-21 21:23:04 -0300

    Add files via upload

---
+++ trab_2/src/Conta.java
@@ -0,0 +1,44 @@
+public class Conta {
+    int numero;
+    float saldo;
+
+    public Conta(int numero, float saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public boolean setSaldo(float saldo) {
+        if(saldo >= 0) {
+            this.saldo = saldo;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void debitaSaldo(float debito){
+        this.saldo = this.saldo - debito;
+    }
+
+    public void creditaSaldo(float saldo){
+        this.saldo = this.saldo + saldo;
+    }
+
+    public String retornaConta(){
+        return "Numero da conta: " + this.numero + "  Saldo: " + this.saldo;
+    }
+
+}
+
