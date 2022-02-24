86c63a7b21734b2216cec10fa8a11e57a813f7e0
lnagatsuyu <leonardo.nagatsuyu@gmail.com>
2022-02-23 22:08:30 -0300

    Add files via upload

--- trab_2/src/Principal.java
+++ trab_2/src/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 
         Scanner read = new Scanner(System.in);
 
+        /*
         System.out.println("Insira o cpf");
         String cpf = read.nextLine();
 
@@ -21,7 +22,7 @@ public class Principal {
 
         Cliente cliente_01 = new Cliente(cpf, anoNascimento, nome, email);
 
-        /*
+
         System.out.println("Insira o numero da conta");
         int numero = read.nextInt();
         */
@@ -30,44 +31,28 @@ public class Principal {
         Conta conta_01 = new Conta(23196, 250);
 
         System.out.println("Insira o novo saldo");
+
         float saldo = read.nextFloat();
         boolean flag = conta_01.setSaldo(saldo);
+
         if(flag){
             System.out.println("Saldo atualizado com sucesso");
         }else{
             System.out.println("Saldo nao atualizado. Nao pode ser negativo");
         }
 
-        System.out.println("Insira o valor a ser debitado: ");
-        float debito = read.nextFloat();
+//        System.out.println("Insira o valor a ser debitado: ");
+//        float debito = read.nextFloat();
+//
+//        conta_01.debitaSaldo(debito);
+//
+//        System.out.println("Insira o valor a ser creditado: ");
+//        float credito = read.nextFloat();
+//
+//        conta_01.creditaSaldo(credito)
+//        System.out.println(cliente_01.retornaCliente());
 
-        conta_01.debitaSaldo(debito);
-
-        System.out.println("Insira o valor a ser creditado: ");
-        float credito = read.nextFloat();
-
-        conta_01.creditaSaldo(credito);
-
-
-
-        System.out.println(cliente_01.retornaCliente());
         System.out.println(conta_01.retornaConta());
-
-
-
-        /*
-        Cliente cliente_01 = new Cliente("02850435902", 1980, "Marcos da Rocha Souza", "mr_souza@gmail.com");
-        Cliente cliente_02 = new Cliente("03510400902", 1985, "Ana Clara Marques", "aninha@gmail.com");
-        Cliente cliente_03 = new Cliente("04564238906", 1975, "Pedro de Alcantara", "pedro_75@gmail.com");
-        Cliente cliente_04 = new Cliente("03510400902", 1985, "Ana Clara Marques", "aninha@gmail.com");
-
-        Conta conta_01 = new Conta(23196, 250);
-        Conta conta_02 = new Conta(1314, 10.36);
-        Conta conta_03 = new Conta(2368, 1523.00);
-        Conta conta_04 = new Conta(3568, -50.45);
-        Conta conta_05 = new Conta(2224, 91.23);
-        Conta conta_06 = new Conta(1019, -70.35);
-        */
 
         read.close();
     }
