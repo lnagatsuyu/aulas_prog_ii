5559619b27970644d2a6a57a6d09c0f615375e4a
lnagatsuyu <leonardo.nagatsuyu@gmail.com>
2022-02-23 22:07:13 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,50 @@
+public class Cliente {
+
+    String cpf;
+    int anoNascimento;
+    String nome;
+    String email;
+
+    public Cliente(String cpf, int anoNascimento, String nome, String email) {
+        this.cpf = cpf;
+        this.anoNascimento = anoNascimento;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String retornaCliente(){
+        return "cpf: " + this.cpf + "  Ano nascimento: " + this.anoNascimento + "  nome: " + this.nome + "  email: " + this.email;
+    }
+}
